3b4e450041f823b3e6959ac3f2abdc36eadcf287
Myles Guanzon <myles.guanzon@omadahealth.com>
2015-10-24 16:02:01 -0700

    Style first page of registration
    - Style landing page
    - Add buton styles
    - Add flash/error styles
    - Add form styles

--- app/controllers/registration/account_controller.rb
+++ app/controllers/registration/account_controller.rb
@@ -1,6 +1,13 @@
 module Registration
   class AccountController < Devise::RegistrationsController
 
+    protected
+
+    # TODO - Edit this path to go to the billing info
+    # def after_sign_up_path_for(resource)
+    #
+    # end
+
     private
 
     def sign_up_params
