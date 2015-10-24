c6394acdbbb79c72beedd87d62eae5f434e54af7
Myles Guanzon <myles.guanzon@omadahealth.com>
2015-10-24 14:55:57 -0700

    Add first and last name to users

--- app/controllers/registration/account_controller.rb
+++ app/controllers/registration/account_controller.rb
@@ -1,4 +1,10 @@
 module Registration
   class AccountController < Devise::RegistrationsController
+
+    private
+
+    def sign_up_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024211246) do
+ActiveRecord::Schema.define(version: 20151024215033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 20151024211246) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.hstore   "roles"
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
