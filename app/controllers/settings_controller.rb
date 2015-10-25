60f02653ff696c23a653b172e2e8140f77142b4e
Myles Guanzon <myles.guanzon@omadahealth.com>
2015-10-24 21:02:43 -0700

    Add ability to update settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,12 +6,28 @@ class SettingsController < ApplicationController
   end
 
   def update
+    current_user.assign_attributes(update_params)
 
+    if current_user.save
+      flash[:notice] = "Account has been updated."
+    else
+      flash[:alert] = "There was a problem upating our account."
+    end
+
+    redirect_to settings_path
   end
 
   private
 
   def update_params
-
+    params.require(:user).permit(
+      :id, :first_name, :last_name, :email,
+      recipient_attributes: [
+        :id, :user_id, :first_name, :last_name,
+        address_attributes: [
+          :id, :recipient_id, :address1, :address2, :city, :state, :zip
+        ]
+      ]
+    )
   end
 end
